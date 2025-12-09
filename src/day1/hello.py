275d00ad63c55746d1b2179935e8343f346d780f
psm128 <71210509+psm128@users.noreply.github.com>
2025-12-09 22:43:06 +0530

    Add initial hello.py script with basic print statements and comments (#1)

---
+++ src/day1/hello.py
@@ -0,0 +1,13 @@
+# Filename: src/L1/S1/01_hello.py
+
+print('Hello, World!')
+print("Welcome to Python Fundamentals!")
+print("This is Session 1")
+
+# This is a single-line comment
+print("\n💬 Comments help explain code")
+
+"""
+This is a multi-line comment. We'll learn more about these in future sessions.
+"""
+print("\n💬 Multi-line comments are useful for longer explanations")
